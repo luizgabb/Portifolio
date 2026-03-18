f686f1bad4fcd84e2ba1ca7eea1d46e17667a13f
luiz.stedile <luiz.stedile@seinfra.ce.gov.br>
2026-03-18 15:19:02 -0300

    finalizando componente educ e deixando responsivo

--- src/app/components/educ/educ.ts
+++ src/app/components/educ/educ.ts
@@ -8,12 +8,16 @@ import { Component } from '@angular/core';
 })
 export class Educ {
   public features = [
-    "The first step involves identifying all places where your personal information might be present.",
-    "Once you've identified where your information is held, the next step is to contact the administrators of this platform",
-    "Removing personal information is not a one-time task but requires ongoing vigilance.",
-    "There are professional services and tools available that specialise in personal information removal.",
-    "There are professional services and tools available that specialise in personal information removal.",
-    "There are professional services and tools available that specialise in personal information removal.",
-    "There are professional services and tools available that specialise in personal information removal."
+    "Uninassau - Análise e Desenvolvimento de Sistemas - Fev 2024 - Jun 2026.",
+    "Super Formação Angular - Ecossistema Angular Completo - 154 horas - Dev Fortin - Cursando.",
+    "DIO - Bootcamp - C&T Backend com Java e AWS - 72 horas - Cursando",
+    "Rocketseat - Angular - Set 2025.",
+    "Rocketseat - Java com Spring Boot - Set 2025.",
+    "Rocketseat - Microserviços com Spring Cloud - Set 2025.",
+    "Fundação Bradesco - Modelagem de Dados - Out 2024.",
+    "Rocketseat - Discover(princípios da web até a criação de aplicações front-end e back-end) - Jun - 2024.",
+    "DIO - Python Essencial e Desenvolvimento Colaborativo com Git - Mai - 2024.",
+    "DIO - Fundamentos da Linguagem de Programação Java - Ago 2025.",
+    "DIO - Versionamento de Código com Git e Github - Ago 2024."
   ];
 }
